e92d7311cfa2d2a64f3a5fe06d87cbd53cef8fce
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-14 16:31:41 +0100

    Fix test to match new component name thing

--- src/test/java/dk/statsbiblioteket/newspaper/md5checker/MD5CheckerComponentTest.java
+++ src/test/java/dk/statsbiblioteket/newspaper/md5checker/MD5CheckerComponentTest.java
@@ -6,25 +6,8 @@ import org.testng.annotations.Test;
 import dk.statsbiblioteket.medieplatform.autonomous.Batch;
 import dk.statsbiblioteket.medieplatform.autonomous.ResultCollector;
 
-/**
- * Test MD5 checker
- */
+/** Test MD5 checker */
 public class MD5CheckerComponentTest {
-
-    private static final String EXPECTED_REPORT = "<result tool=\"MD5_checker_component\" version=\"0.1\" xmlns=\"http://schemas.statsbiblioteket.dk/result/\">\n"+
-                                                  "    <outcome>Failure</outcome>\n" +
-                                                  "    <date>[date]</date>\n" +
-                                                  "    <failures>\n" +
-                                                  "        <failure>\n" +
-                                                  "            <filereference>B400022028241-RT1/400022028241-14/1795-06-13-01/AdresseContoirsEfterretninger-1795-06-13-01-0006-brik.jp2/contents</filereference>\n"
-                                                  +
-                                                  "            <type>checksum</type>\n" +
-                                                  "            <component>MD5_checker_component-0.1</component>\n" +
-                                                  "            <description>Expected checksum d41d8cd98f00b204e9800998ecf8427f, but was d41d8cd98f00b204e9800998ecf8427e</description>\n"
-                                                  +
-                                                  "        </failure>\n" +
-                                                  "    </failures>\n" +
-                                                  "</result>";
 
     @Test
     /**
@@ -34,15 +17,41 @@ public class MD5CheckerComponentTest {
         MD5CheckerComponent md5CheckerComponent = new MockupIteratorSuper(System.getProperties());
 
         // Run on first batch with one wrong checksum
-        ResultCollector result =
-                new ResultCollector(md5CheckerComponent.getComponentName(), md5CheckerComponent.getComponentVersion());
+        ResultCollector result = new ResultCollector(
+                md5CheckerComponent.getComponentName(),
+                md5CheckerComponent.getComponentVersion());
         Batch batch = new Batch("400022028241");
         batch.setRoundTripNumber(1);
         md5CheckerComponent.doWorkOnBatch(batch, result);
 
         // Assert one failure with the expected report
         Assert.assertFalse(result.isSuccess(), result.toReport() + "\n");
-        Assert.assertEquals(result.toReport().replaceAll("<date>[^<]*</date>", "<date>[date]</date>"), EXPECTED_REPORT);
+        String expected = "<result tool=\"" + md5CheckerComponent.getComponentName() + "\" xmlns=\"http://schemas.statsbiblioteket.dk/result/\">\n"
+                          +
+                          "    <outcome>Failure</outcome>\n"
+                          +
+                          "    <date>[date]</date>\n"
+                          +
+                          "    <failures>\n"
+                          +
+                          "        <failure>\n"
+                          +
+                          "            <filereference>B400022028241-RT1/400022028241-14/1795-06-13-01/AdresseContoirsEfterretninger-1795-06-13-01-0006-brik.jp2/contents</filereference>\n"
+                          +
+                          "            <type>checksum</type>\n"
+                          +
+                          "            <component>" + md5CheckerComponent.getComponentName() + "-" + md5CheckerComponent.getComponentVersion() + "</component>\n"
+                          +
+                          "            <description>Expected checksum d41d8cd98f00b204e9800998ecf8427f, but was d41d8cd98f00b204e9800998ecf8427e</description>\n"
+                          +
+                          "        </failure>\n"
+                          +
+                          "    </failures>\n"
+                          +
+                          "</result>";
+        Assert.assertEquals(
+                result.toReport()
+                      .replaceAll("<date>[^<]*</date>", "<date>[date]</date>"), expected);
 
         // Run on second batch with no wrong checksums
         result = new ResultCollector(md5CheckerComponent.getComponentName(), md5CheckerComponent.getComponentVersion());
