64e2bbf0ba56248b34b5cd09fba8365830ee95c0
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2013-12-10 13:47:38 +0100

    Update to match new framework interface, and return status code on failure

--- src/main/java/dk/statsbiblioteket/newspaper/md5checker/MD5Checker.java
+++ src/main/java/dk/statsbiblioteket/newspaper/md5checker/MD5Checker.java
@@ -3,6 +3,7 @@ package dk.statsbiblioteket.newspaper.md5checker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dk.statsbiblioteket.medieplatform.autonomous.CallResult;
 import dk.statsbiblioteket.medieplatform.autonomous.RunnableComponent;
 import dk.statsbiblioteket.medieplatform.autonomous.AutonomousComponentUtils;
 
@@ -23,9 +24,19 @@ public class MD5Checker {
      * @throws Exception
      * @see AutonomousComponentUtils#parseArgs(String[])
      */
-    public static void main(String[] args)
-            throws
-            Exception {
+    public static void main(String... args) throws Exception {
+        System.exit(doMain(args));
+    }
+
+    /**
+     * The class must have a main method, so it can be started as a command line tool
+     *
+     * @param args the arguments.
+     *
+     * @throws Exception
+     * @see AutonomousComponentUtils#parseArgs(String[])
+     */
+    public static int doMain(String[] args) throws Exception {
         log.info("Starting with args {}", args);
 
         //Parse the args to a properties construct
@@ -34,8 +45,9 @@ public class MD5Checker {
         //make a new runnable component from the properties
         RunnableComponent component = new MD5CheckerComponent(properties);
 
-        Map<String, Boolean> result = AutonomousComponentUtils.startAutonomousComponent(properties, component);
+        CallResult result = AutonomousComponentUtils.startAutonomousComponent(properties, component);
 
-        AutonomousComponentUtils.printResults(result);
+        System.out.println(result);
+        return result.containsFailures();
     }
 }
